4a4991930a78c6ab8c3a5f429b1e3061e8790114
pavel.melamed <pavel.melamed@worldapp.com>
2021-12-25 21:04:23 +0200

    Year 2021 Day 24 minimal

--- src/aoc2021/Day24Y21.java
+++ src/aoc2021/Day24Y21.java
@@ -3,37 +3,14 @@ package aoc2021;
 import common.AocDay;
 import common.Utils;
 
-import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.function.BiPredicate;
-import java.util.function.Consumer;
-import java.util.stream.Collectors;
 
 public class Day24Y21 implements AocDay<Long, Long> {
-    public static final Map<Character, Integer> REGNAMES = Map.of( 'w', 0, 'x', 1, 'y', 2, 'z', 3 );
     private static final int[] DELTA_X = { 10, 13, 12, -12, 11, -13, -9, -12, 14, -9, 15, 11, -16, -2 };
     private static final int[] DELTA_Y = { 5, 9, 4, 4, 10, 14, 14, 12, 14, 14, 5, 10, 8, 15 };
     private final String name;
-    private final List<Consumer<ALUState>> instructions;
-
-    private static class ALUState {
-        private final long[] regs = new long[4];
-        private final long[] input;
-        private int inputPtr = 0;
-
-        public ALUState( long[] input ) {
-            this.input = input;
-        }
-
-        public long readInput() {
-            return input[inputPtr++];
-        }
-
-        private boolean isValid() {
-            return regs[3] == 0;
-        }
-    }
 
     public static void main( String[] args ) {
         try {
@@ -52,14 +29,7 @@ public class Day24Y21 implements AocDay<Long, Long> {
     }
 
     public Day24Y21( String file ) {
-        this( Utils.readLines( file ), file );
-    }
-
-    public Day24Y21( List<String> sample, String name ) {
-        this.name = name;
-        instructions = sample.stream()
-                             .map( Day24Y21::parseInstruction )
-                             .collect( Collectors.toList() );
+        this.name = file;
     }
 
     @Override
@@ -68,17 +38,11 @@ public class Day24Y21 implements AocDay<Long, Long> {
     }
 
     public Long task1() {
-        return solve( ( a, b ) -> a > b );
+        return getExtremeNumber( ( a, b ) -> a > b );
     }
 
     public Long task2() {
-        return solve( ( a, b ) -> a < b );
-    }
-
-    private long solve( BiPredicate<Long, Long> replacePredicate ) {
-        long number = getExtremeNumber( replacePredicate );
-        System.out.printf( "%d - %s", number, executeMonadProgram( numberToInput( number ) ) ? "OK" : "Fail" );
-        return number;
+        return getExtremeNumber( ( a, b ) -> a < b );
     }
 
     private static long getExtremeNumber( BiPredicate<Long, Long> replacePredicate ) {
@@ -123,101 +87,5 @@ public class Day24Y21 implements AocDay<Long, Long> {
             }
         }
         return z;
-    }
-
-    private static long[] numberToInput( long number ) {
-        long[] input = new long[14];
-        for ( int pos = 13; pos >= 0; --pos ) {
-            input[pos] = number % 10;
-            number /= 10;
-        }
-        return input;
-    }
-
-    private boolean executeMonadProgram( long[] input ) {
-        ALUState alu = new ALUState( input );
-        instructions.forEach( instr -> instr.accept( alu ) );
-        return alu.isValid();
-    }
-
-    private static Consumer<ALUState> parseInstruction( String text ) {
-        String[] args = text.split( " " );
-        int srcReg = REGNAMES.get( args[1].charAt( 0 ) );
-        if ( args.length == 3 ) {
-            Integer dstReg = REGNAMES.get( args[2].charAt( 0 ) );
-            if ( dstReg != null ) {
-                return mapRegInstruction( args[0], srcReg, dstReg );
-            } else {
-                return mapNumberInstruction( args[0], srcReg, Long.parseLong( args[2] ) );
-            }
-        } else {
-            return alu -> doInput( alu, srcReg );
-        }
-    }
-
-    private static Consumer<ALUState> mapNumberInstruction( String instr, int reg, long number ) {
-        return switch ( instr ) {
-            case "add" -> alu -> doAddNumber( alu, reg, number );
-            case "mul" -> alu -> doMulNumber( alu, reg, number );
-            case "div" -> alu -> doDivNumber( alu, reg, number );
-            case "mod" -> alu -> doModNumber( alu, reg, number );
-            case "eql" -> alu -> doEqlNumber( alu, reg, number );
-            default -> throw new IllegalStateException( "bad instruction " + instr );
-        };
-    }
-
-    private static Consumer<ALUState> mapRegInstruction( String instr, int src, int dst ) {
-        return switch ( instr ) {
-            case "add" -> alu -> doAddReg( alu, src, dst );
-            case "mul" -> alu -> doMulReg( alu, src, dst );
-            case "div" -> alu -> doDivReg( alu, src, dst );
-            case "mod" -> alu -> doModReg( alu, src, dst );
-            case "eql" -> alu -> doEqlReg( alu, src, dst );
-            default -> throw new IllegalStateException( "Bad instruction " + instr );
-        };
-    }
-
-    private static void doInput( ALUState alu, int reg ) {
-        alu.regs[reg] = alu.readInput();
-    }
-
-    private static void doAddNumber( ALUState alu, int reg, long number ) {
-        alu.regs[reg] += number;
-    }
-
-    private static void doAddReg( ALUState alu, int dst, int src ) {
-        alu.regs[dst] += alu.regs[src];
-    }
-
-    private static void doMulNumber( ALUState alu, int reg, long number ) {
-        alu.regs[reg] *= number;
-    }
-
-    private static void doMulReg( ALUState alu, int dst, int src ) {
-        alu.regs[dst] *= alu.regs[src];
-    }
-
-    private static void doDivNumber( ALUState alu, int reg, long number ) {
-        alu.regs[reg] /= number;
-    }
-
-    private static void doDivReg( ALUState alu, int dst, int src ) {
-        alu.regs[dst] /= alu.regs[src];
-    }
-
-    private static void doModNumber( ALUState alu, int reg, long number ) {
-        alu.regs[reg] %= number;
-    }
-
-    private static void doModReg( ALUState alu, int dst, int src ) {
-        alu.regs[dst] %= alu.regs[src];
-    }
-
-    private static void doEqlNumber( ALUState alu, int reg, long number ) {
-        alu.regs[reg] = alu.regs[reg] == number ? 1L : 0L;
-    }
-
-    private static void doEqlReg( ALUState alu, int dst, int src ) {
-        alu.regs[dst] = alu.regs[src] == alu.regs[dst] ? 1L : 0L;
     }
 }

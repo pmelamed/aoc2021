159297f2b88153510b51307b892d50b0f81c5846
Pavel Melamed <pavel.melamed@form.com>
2023-12-25 02:21:26 +0100

    2023 Day 18 - part 2

--- src/aoc2023/Day18Y23.java
+++ src/aoc2023/Day18Y23.java
@@ -4,6 +4,7 @@ import common.AocDay;
 import common.Utils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.TreeSet;
@@ -11,20 +12,14 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Day18Y23 implements AocDay<Long, Long> {
-    private static final int DIR_NORTH = 0;
-    private static final int DIR_EAST = 1;
-    private static final int DIR_SOUTH = 2;
-    private static final int DIR_WEST = 3;
+    private static final int DIR_NONE = 0;
+    private static final int DIR_NORTH = 1;
+    private static final int DIR_EAST = 2;
+    private static final int DIR_SOUTH = 3;
+    private static final int DIR_WEST = 4;
 
-    private static final int LINE_NONE = 0;
-    private static final int LINE_UP = 1;
-    private static final int LINE_DOWN = 2;
-    private static final int LINE_HORIZONTAL = 3;
-
-    private static final int FILL = 4;
-
-    private static final int[] DX = { 0, 1, 0, -1 };
-    private static final int[] DY = { -1, 0, 1, 0 };
+    private static final int[] DX = { 0, 0, 1, 0, -1 };
+    private static final int[] DY = { 0, -1, 0, 1, 0 };
     private static final Pattern COMMAND_PATTERN = Pattern.compile( "([UDLR]) ([0-9]+) \\(#([0-9a-z]{6})\\)" );
 
     // Fill FSM
@@ -34,32 +29,36 @@ public class Day18Y23 implements AocDay<Long, Long> {
     private static final int MODE_ENTER_LINE_DOWN = 3;
     private static final int MODE_EXIT_LINE_DOWN = 4;
 
-    private static final int[][] FSM = new int[5][4];
+    private static final int[][] FSM = new int[5][5];
 
     static {
-        FSM[MODE_EMPTY][LINE_NONE] = MODE_EMPTY;
-        FSM[MODE_EMPTY][LINE_UP] = MODE_ENTER_LINE_UP;
-        FSM[MODE_EMPTY][LINE_DOWN] = MODE_ENTER_LINE_DOWN;
+        FSM[MODE_EMPTY][DIR_NONE] = MODE_EMPTY;
+        FSM[MODE_EMPTY][DIR_NORTH] = MODE_ENTER_LINE_UP;
+        FSM[MODE_EMPTY][DIR_SOUTH] = MODE_ENTER_LINE_DOWN;
 
-        FSM[MODE_ENTER_LINE_UP][LINE_UP] = MODE_ENTER_LINE_UP;
-        FSM[MODE_ENTER_LINE_UP][LINE_HORIZONTAL] = MODE_ENTER_LINE_UP;
-        FSM[MODE_ENTER_LINE_UP][LINE_NONE] = MODE_ENTER_LINE_UP;
-        FSM[MODE_ENTER_LINE_UP][LINE_DOWN] = MODE_EXIT_LINE_DOWN;
+        FSM[MODE_ENTER_LINE_UP][DIR_NONE] = MODE_ENTER_LINE_UP;
+        FSM[MODE_ENTER_LINE_UP][DIR_NORTH] = MODE_ENTER_LINE_UP;
+        FSM[MODE_ENTER_LINE_UP][DIR_EAST] = MODE_ENTER_LINE_UP;
+        FSM[MODE_ENTER_LINE_UP][DIR_SOUTH] = MODE_EXIT_LINE_DOWN;
+        FSM[MODE_ENTER_LINE_UP][DIR_WEST] = MODE_ENTER_LINE_UP;
 
-        FSM[MODE_ENTER_LINE_DOWN][LINE_DOWN] = MODE_ENTER_LINE_DOWN;
-        FSM[MODE_ENTER_LINE_DOWN][LINE_HORIZONTAL] = MODE_ENTER_LINE_DOWN;
-        FSM[MODE_ENTER_LINE_DOWN][LINE_NONE] = MODE_ENTER_LINE_DOWN;
-        FSM[MODE_ENTER_LINE_DOWN][LINE_UP] = MODE_EXIT_LINE_UP;
+        FSM[MODE_ENTER_LINE_DOWN][DIR_NONE] = MODE_ENTER_LINE_DOWN;
+        FSM[MODE_ENTER_LINE_DOWN][DIR_NORTH] = MODE_EXIT_LINE_UP;
+        FSM[MODE_ENTER_LINE_DOWN][DIR_EAST] = MODE_ENTER_LINE_DOWN;
+        FSM[MODE_ENTER_LINE_DOWN][DIR_SOUTH] = MODE_ENTER_LINE_DOWN;
+        FSM[MODE_ENTER_LINE_DOWN][DIR_WEST] = MODE_ENTER_LINE_DOWN;
 
-        FSM[MODE_EXIT_LINE_UP][LINE_UP] = MODE_EXIT_LINE_UP;
-        FSM[MODE_EXIT_LINE_UP][LINE_HORIZONTAL] = MODE_EXIT_LINE_UP;
-        FSM[MODE_EXIT_LINE_UP][LINE_NONE] = MODE_EMPTY;
-        FSM[MODE_EXIT_LINE_UP][LINE_DOWN] = MODE_ENTER_LINE_DOWN;
+        FSM[MODE_EXIT_LINE_UP][DIR_NONE] = MODE_EMPTY;
+        FSM[MODE_EXIT_LINE_UP][DIR_NORTH] = MODE_EXIT_LINE_UP;
+        FSM[MODE_EXIT_LINE_UP][DIR_EAST] = MODE_EXIT_LINE_UP;
+        FSM[MODE_EXIT_LINE_UP][DIR_SOUTH] = MODE_ENTER_LINE_DOWN;
+        FSM[MODE_EXIT_LINE_UP][DIR_WEST] = MODE_EXIT_LINE_UP;
 
-        FSM[MODE_EXIT_LINE_DOWN][LINE_DOWN] = MODE_EXIT_LINE_DOWN;
-        FSM[MODE_EXIT_LINE_DOWN][LINE_HORIZONTAL] = MODE_EXIT_LINE_DOWN;
-        FSM[MODE_EXIT_LINE_DOWN][LINE_NONE] = MODE_EMPTY;
-        FSM[MODE_EXIT_LINE_DOWN][LINE_UP] = MODE_ENTER_LINE_UP;
+        FSM[MODE_EXIT_LINE_DOWN][DIR_NONE] = MODE_EMPTY;
+        FSM[MODE_EXIT_LINE_DOWN][DIR_NORTH] = MODE_ENTER_LINE_UP;
+        FSM[MODE_EXIT_LINE_DOWN][DIR_EAST] = MODE_EXIT_LINE_DOWN;
+        FSM[MODE_EXIT_LINE_DOWN][DIR_SOUTH] = MODE_EXIT_LINE_DOWN;
+        FSM[MODE_EXIT_LINE_DOWN][DIR_WEST] = MODE_EXIT_LINE_DOWN;
     }
 
     private final String filename;
@@ -67,8 +66,7 @@ public class Day18Y23 implements AocDay<Long, Long> {
     public static void main( String[] args ) {
         try {
             Utils.executeSampleDay( new Day18Y23( "input/2023/Y23D18S1.dat" ), 62L, 952_408_144_115L );
-//            Utils.executeSampleDay( new Day18Y23( "input/2023/Y23D18S2.dat" ), null, null );
-            Utils.executeDay( new Day18Y23( "input/2023/Y23D18I.dat" ), 62573L, null );
+            Utils.executeDay( new Day18Y23( "input/2023/Y23D18I.dat" ), 62573L, 54_662_804_037_719L );
         } catch ( Throwable e ) {
             e.printStackTrace();
         }
@@ -107,18 +105,13 @@ public class Day18Y23 implements AocDay<Long, Long> {
         x = -minx;
         y = -miny;
         for ( Command cmd : commands ) {
-            int lineType = switch ( cmd.dir() ) {
-                case DIR_NORTH -> LINE_UP;
-                case DIR_SOUTH -> LINE_DOWN;
-                default -> LINE_HORIZONTAL;
-            };
             if ( cmd.dir() == DIR_NORTH || cmd.dir() == DIR_SOUTH ) {
-                lines[y][x] = lineType;
+                lines[y][x] = cmd.dir();
             }
             for ( int i = 0; i < cmd.length(); i++ ) {
                 x += DX[cmd.dir()];
                 y += DY[cmd.dir()];
-                lines[y][x] = lineType;
+                lines[y][x] = cmd.dir();
             }
         }
         long count = 0;
@@ -148,20 +141,59 @@ public class Day18Y23 implements AocDay<Long, Long> {
             switch ( cmd.dir() ) {
                 case DIR_NORTH:
                 case DIR_SOUTH:
-                    horizontalEdges.add( new Edge( x, xnext, y, cmd.dir() ) );
+                    verticalEdges.add( new Edge( y, ynext, x, cmd.dir() ) );
                     break;
                 case DIR_EAST:
                 case DIR_WEST:
-                    verticalEdges.add( new Edge( y, ynext, x, cmd.dir() ) );
+                    horizontalEdges.add( new Edge( x, xnext, y, cmd.dir() ) );
                     break;
             }
             x = xnext;
             y = ynext;
         }
-        Row[] verticalRows = makeRows( verticalEdges );
-        Row[] horizontalRows = makeRows( horizontalEdges );
-        int[][] field = new int[verticalRows.length][horizontalRows.length];
-        return null;
+        Row[] rows = makeRows( verticalEdges );
+        Row[] columns = makeRows( horizontalEdges );
+        int[] rowStarts = Arrays.stream( rows ).mapToInt( Row::start ).toArray();
+        int[] columnStarts = Arrays.stream( columns ).mapToInt( Row::start ).toArray();
+        int[][] lines = new int[rows.length][columns.length];
+        for ( Edge edge : horizontalEdges ) {
+            int startCol = Arrays.binarySearch( columnStarts, edge.start() );
+            int endCol = Arrays.binarySearch( columnStarts, edge.end() );
+            if ( startCol > endCol ) {
+                int tmp = startCol;
+                startCol = endCol;
+                endCol = tmp;
+            }
+            int row = Arrays.binarySearch( rowStarts, edge.coord() );
+            for ( int col = startCol + 1; col < endCol; col++ ) {
+                lines[row][col] = edge.dir();
+            }
+        }
+        for ( Edge edge : verticalEdges ) {
+            int startRow = Arrays.binarySearch( rowStarts, edge.start() );
+            int endRow = Arrays.binarySearch( rowStarts, edge.end() );
+            if ( startRow > endRow ) {
+                int tmp = startRow;
+                startRow = endRow;
+                endRow = tmp;
+            }
+            int column = Arrays.binarySearch( columnStarts, edge.coord() );
+            for ( int row = startRow; row <= endRow; row++ ) {
+                lines[row][column] = edge.dir();
+            }
+        }
+        long count = 0;
+        for ( int row = 0; row < rows.length; row++ ) {
+            int mode = MODE_EMPTY;
+            int rowSize = rows[row].size;
+            for ( int col = 0; col < columns.length; col++ ) {
+                mode = FSM[mode][lines[row][col]];
+                if ( mode != MODE_EMPTY ) {
+                    count += (long) rowSize * columns[col].size;
+                }
+            }
+        }
+        return count;
     }
 
     private Row[] makeRows( Collection<Edge> edges ) {
@@ -178,6 +210,7 @@ public class Day18Y23 implements AocDay<Long, Long> {
                 rows.add( new Row( prevValue + 1, value - prevValue - 1 ) );
             }
             rows.add( new Row( value, 1 ) );
+            prevValue = value;
         }
         return rows.toArray( Row[]::new );
     }
@@ -207,14 +240,14 @@ public class Day18Y23 implements AocDay<Long, Long> {
         if ( !matcher.find() ) {
             throw new IllegalArgumentException( "Bad command <%s>".formatted( line ) );
         }
-        int code = Integer.parseInt( matcher.group( 2 ), 0x10 );
+        int code = Integer.parseInt( matcher.group( 3 ), 0x10 );
         int dir = switch ( code & 0xF ) {
             case 0 -> DIR_WEST;
             case 1 -> DIR_SOUTH;
             case 2 -> DIR_EAST;
             case 3 -> DIR_NORTH;
             default -> throw new IllegalArgumentException(
-                    "Bad command <%s>: direction %s".formatted( line, matcher.group( 1 ) )
+                    "Bad command <%s>: direction %s".formatted( line, code & 0xF )
             );
         };
         return new Command(
